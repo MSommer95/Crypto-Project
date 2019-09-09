9fbecf1f1a35cf0dcb460425cf8b3975dc12f599
MSommer95 <maximilian.sommer@stud.hshl.de>
2019-09-09 15:51:13 +0200

    email_sender basics file_encryption basics start_script basics

--- server/python/start_script.py
+++ server/python/start_script.py
@@ -1,1 +1,13 @@
-#Start Script empty
+
+import cherrypy
+
+
+class Index(object):
+
+    @cherrypy.expose()
+    def index(self):
+        return open('../index.html')
+
+
+if __name__ == '__main__':
+    cherrypy.quickstart(Index())

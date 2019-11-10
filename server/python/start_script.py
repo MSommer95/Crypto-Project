da8d1cf8cfd97d1b8551027410b985729c833202
dNNs94 <dennis.liesack@gmx.de>
2019-11-10 14:34:23 +0100

    refactored authenticate_app to use json data

--- server/python/start_script.py
+++ server/python/start_script.py
@@ -140,7 +140,14 @@ class Index(object):
 
     @cherrypy.expose()
     @cherrypy.tools.json_out()
-    def authenticate_app(self, email, password, device_id):
+    @cherrypy.tools.json_in()
+    def authenticate_app(self):
+        input_json = cherrypy.request.json
+
+        email = input_json['email']
+        password = input_json['password']
+        device_id = input_json['device_id']
+
         user = DbConnector.db_check_user(email, password)
         user_count = len(user)
         cherrypy.serving.response.headers['Content-Type'] = 'application/json'
